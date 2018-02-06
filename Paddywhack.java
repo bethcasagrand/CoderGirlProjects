f3235e5226b9129fb27fb1e6f177b7617f6ef193
bethcasagrand <35548471+bethcasagrand@users.noreply.github.com>
2018-02-06 09:39:37 -0600

    Add files via upload

---
+++ Paddywhack.java
@@ -0,0 +1,58 @@
+public class Paddywhack
+{
+
+        public static void verse1()
+        {
+            System.out.print("This old man, he played ");
+        }
+        public static void verse2()
+        {
+            System.out.print("He played knick-knack on my ");
+        }
+        public static void verse3()
+        {
+            System.out.println("With a knick-knack paddywhack,\n" +
+                    "Give the dog a bone,\n" +
+                    "This old man came rolling home.\n");
+        }
+
+        public static void main(String[] args)
+        {
+            verse1();
+            System.out.println("one.");
+            verse2();
+            System.out.println("thumb.");
+            verse3();
+
+            verse1();
+            System.out.println("two.");
+            verse2();
+            System.out.println("shoe.");
+            verse3();
+
+            verse1();
+            System.out.println("three.");
+            verse2();
+            System.out.println("knee.");
+            verse3();
+
+            verse1();
+            System.out.println("four.");
+            verse2();
+            System.out.println("door.");
+            verse3();
+
+            verse1();
+            System.out.println("five.");
+            verse2();
+            System.out.println("hive.");
+            verse3();
+
+            verse1();
+            System.out.println("six.");
+            verse2();
+            System.out.println("sticks.");
+            verse3();
+        }
+
+}
